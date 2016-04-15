81aa2b803ad1a1bdef1bf99c5714a370fe4b09d9
Edward-Shaw <yxiao@aliyun.com>
2016-04-15 18:18:10 +0800

    add Author and Article model.

--- buddha/teachings/models.py
+++ buddha/teachings/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    id = models.UUIDField()
+    name = models.CharField(max_length=256)
+
+class Article(models.Model):
+    id = models.UUIDField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    create_date = models.DateTimeField('date published')
+    title = models.CharField(max_length=256)
+    content = models.CharField(max_length=4096)
+
