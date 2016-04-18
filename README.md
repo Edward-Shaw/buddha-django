57db950e9dcb5467b9ee8371344426349dd42f54
Edward-Shaw <yxiao@aliyun.com>
2016-04-18 20:18:33 +0800

    activaing models

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ https://docs.djangoproject.com/en/1.9/intro/tutorial02/
 
 ## best practice
 https://docs.djangoproject.com/en/1.9/misc/design-philosophies/#dry
+
+## Tips
+### 1.Three-step guide to making model changes:
+``` python
+change your models(in models.py).
+Run 'python manage.py makemigrations' to create migrations for those changes.
+Run 'python manage.py migrate' to apply those changes to the database.
+```
--- buddha/teachings/models.py
+++ buddha/teachings/models.py
@@ -3,11 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Author(models.Model):
-    id = models.UUIDField()
+    id = models.UUIDField(primary_key=True)
     name = models.CharField(max_length=256)
 
 class Article(models.Model):
-    id = models.UUIDField()
+    id = models.UUIDField(primary_key=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     create_date = models.DateTimeField('date published')
     title = models.CharField(max_length=256)
